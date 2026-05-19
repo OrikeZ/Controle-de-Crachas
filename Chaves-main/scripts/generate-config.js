59696fed3476cffd60cd12d4b0375b4014567884
Vinicius Ouriques <37411193+OrikeZ@users.noreply.github.com>
2026-05-19 11:59:08 -0300

    Delete Chaves-main directory

--- Chaves-main/scripts/generate-config.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const url = (process.env.API_URL || '').replace(/\\/g, '\\\\').replace(/'/g, "\\'");
-const conteudo = `window.API_BASE_URL = '${url}';\n`;
-const destino = path.join(__dirname, '..', 'config.js');
-
-fs.writeFileSync(destino, conteudo, 'utf8');
-console.log(`config.js gerado com API_BASE_URL = "${process.env.API_URL || ''}"`);
